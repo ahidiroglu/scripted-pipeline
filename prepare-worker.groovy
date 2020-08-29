c830b90eadf270182b50c166f4b7764df52a0341
Ali Hidiroglu <kutayhidiroglu@gmail.com>
2020-08-29 12:34:22 -0500

    changes

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,7 +1,26 @@
-node {
-    stage('Init') {
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        ])
+    ])
+if (nodeIP?.trim()) {
+    node {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@159.65.234.153 yum install epel-release -y'
+            stage('Init') {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install epel-release -y'
+            }
+            stage("Install git") {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install git -y'
+            }
+            stage("Install Java"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+            }
+            stage("Install Ansible"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+            }
         }
     }
+}
+else {
+    error 'Please enter valid IP address'
 }
